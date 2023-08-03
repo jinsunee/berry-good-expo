5f37f196d40f75db85788548f11cb116fcfbd135
jinsunee <jsunee.dev@email.com>
2023-08-03 11:56:39 +0900

    feat: tabBarIcon 적용

--- metro.config.js
+++ metro.config.js
@@ -4,6 +4,19 @@ const { getDefaultConfig } = require("expo/metro-config");
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname, { isCSSEnabled: true });
 
+const { transformer, resolver } = config;
+
+config.transformer = {
+  ...transformer,
+  babelTransformerPath: require.resolve("react-native-svg-transformer"),
+};
+
+config.resolver = {
+  ...resolver,
+  assetExts: resolver.assetExts.filter((ext) => ext !== "svg"),
+  sourceExts: [...resolver.sourceExts, "svg"],
+};
+
 config.resolver.assetExts.push("db");
 
 module.exports = config;
--- src/components/navigators/HomeTabNavigator.tsx
+++ src/components/navigators/HomeTabNavigator.tsx
@@ -1,21 +1,37 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import CalendarSvg from "assets/svgs/calendar.svg";
+import SettingSvg from "assets/svgs/setting.svg";
 import Settings from "components/pages/Settings";
 import HomeStackNavigator from "./HomeStackNavigator";
 
 const Tab = createBottomTabNavigator();
 
+const getTabIconColor = (isFocused: boolean) => {
+  return isFocused ? "#000" : "#E1DEDE";
+};
+
 export default function HomeTabNavigator() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator screenOptions={{ tabBarShowLabel: false }}>
       <Tab.Screen
         name="HomeStack"
         component={HomeStackNavigator}
-        options={{ headerShown: false }}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <CalendarSvg fill={getTabIconColor(focused)} />
+          ),
+        }}
       />
       <Tab.Screen
         name="Settings"
         component={Settings}
-        options={{ headerShown: false }}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <SettingSvg fill={getTabIconColor(focused)} />
+          ),
+        }}
       />
     </Tab.Navigator>
   );
